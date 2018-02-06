687cd86ca25f142117763a58d5c25e80ecc76da4
Gwyneth <gwenalbania11@gmail.com>
2018-02-06 15:35:32 +0800

    Added Gender (Admission)

--- saveadmission.php
+++ saveadmission.php
@@ -36,6 +36,7 @@ $mname = $_POST['txtStudMname'];
 $bday = $_POST['txtStudBday'];
 $bplace= $_POST['txtStudBplace'];
 $nat = $_POST['txtStudNat'];
+$gender = $_POST['txtStudGender'];
 $religion = $_POST['txtStudReligion'];
 $addBldg = $_POST['txtStudAddBldg'];
 $addSt = $_POST['txtStudAddSt'];
@@ -177,7 +178,7 @@ $result = mysqli_query($con, $query);
 $row = mysqli_fetch_assoc($result);
 $infoid = $row['tblStudInfoId'];
 $infoid ++;
-$query="insert into tblstudentinfo(tblStudInfoId, tblStudInfoFname, tblStudInfoLname, tblStudInfoMname, tblStudInfoBday, tblStudInfoBplace, tblStudInfoNationality, tblStudInfoReligion, tblStudInfoLang1, tblStudInfoLang2, tblStudInfo_tblStudentId, tblStudInfoAddBldg, tblStudInfoAddSt, tblStudInfoAddBrgy, tblStudInfoAddCity, tblStudInfoAddCountry) values ('$infoid', '$fname', '$lname', '$mname', '$bday', '$bplace', '$nat', '$religion', '$lang1', '$lang2', '$studentid', '$addBldg', '$addSt', '$addBrgy', '$addCity', '$addCountry')";
+$query="insert into tblstudentinfo(tblStudInfoId, tblStudInfoFname, tblStudInfoLname, tblStudInfoMname, tblStudInfoBday, tblStudInfoBplace, tblStudInfoGender, tblStudInfoNationality, tblStudInfoReligion, tblStudInfoLang1, tblStudInfoLang2, tblStudInfo_tblStudentId, tblStudInfoAddBldg, tblStudInfoAddSt, tblStudInfoAddBrgy, tblStudInfoAddCity, tblStudInfoAddCountry) values ('$infoid', '$fname', '$lname', '$mname', '$bday', '$bplace', '$gender' '$nat', '$religion', '$lang1', '$lang2', '$studentid', '$addBldg', '$addSt', '$addBrgy', '$addCity', '$addCountry')";
 if (!$query = mysqli_query($con, $query)) {
 	   exit(mysqli_error($con));
 	}
